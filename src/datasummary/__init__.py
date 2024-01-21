954621362eb40e56dbb2179eca31558ee8b96cc5
Niall Keleher <niall.keleher@gmail.com>
2024-01-21 09:58:41 -0800

    init datasummary_skim method

--- src/datasummary/__init__.py
+++ src/datasummary/__init__.py
@@ -1,1 +1,10 @@
-__version__ = "0.1.0"
+# Define datasummary version
+from importlib_metadata import version as _v
+
+# __version__ = "0.1.0"
+__version__ = _v("great_tables")
+
+del _v
+
+# Import datasummary objects
+from .ds import *  # noqa: F401, F403, E402
