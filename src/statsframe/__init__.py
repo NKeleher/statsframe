ed12a4b20fdffff03d4d6796e91cd9aaf66f9a7a
Niall Keleher <niall.keleher@gmail.com>
2024-02-01 20:14:42 -0800

    add dynamic-versioning

--- src/statsframe/__init__.py
+++ src/statsframe/__init__.py
@@ -1,5 +1,5 @@
 # Define statsframe version
-
+__version__ = "0.0.0"
 # Import statsframe objects
 # from ._tbl_data import *  # noqa: F401, F403, E402
 # from ._databackend import *  # noqa: F401, F403, E402
